501d888dfc7f3a3d1ff6d5460ac7dc5fbd223831
Áine Thompson <ainegthompson@gmail.com>
2021-02-12 18:40:58 +0000

    Complete all operations

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 """CLI application for a prefix-notation calculator."""
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
-                        power, mod, )
+                        power, mod, add_mult, add_cubes)
 
 
 # repeat forever:
@@ -19,20 +19,50 @@ while True:
     tokens = input_str.split(' ')
   
     if tokens[0] == "q":
-        quit
+        quit()
+    elif len(tokens) < 2:
+        print("Not enough inputs.")
+        continue
+   
+    operator = tokens[0]
+    num1 = float(tokens[1])
+
+    if len(tokens) < 3:
+        num2 = 0
     else:
-        operator = tokens[0]
-        num1 = float(tokens[1])
         num2 = float(tokens[2])
 
-        if operator == "+":
-            print(add(num1, num2))
-        elif operator == "-":
-            print(subtract(num1, num2))
-        elif operator == "*":
-            multiply(num1, num2)
-        elif operator == "/":
-            divide(num1, num2)
-        elif operator == "pow":
-            power(num1, num2)
+    if len(tokens) > 3:
+        num3 = float(tokens[3])
+
+    if operator == "+":
+        result = add(num1, num2)
+
+    elif operator == "-":
+        result = subtract(num1, num2)
+
+    elif operator == "*":
+        result = multiply(num1, num2)
         
+    elif operator == "/":
+        result = divide(num1, num2)
+
+    elif operator == "square":
+        result = square(num1)
+
+    elif operator == "cube":
+        result = cube(num1)
+
+    elif operator == "pow":
+        result = power(num1, num2)
+
+    elif operator == "mod":
+        result = mod(num1, num2)
+
+    elif operator == "x+":
+        result = add_mult(num1, num2, num3)
+
+    elif operator == "cubes+":
+        result = add_cubes(num1, num2)
+
+    print(result)
