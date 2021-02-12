1509c355ac481bb006b3eda5d70688030e74a3fc
Áine Thompson <ainegthompson@gmail.com>
2021-02-12 00:34:03 +0000

    Pseudocode

--- calculator.py
+++ calculator.py
@@ -3,5 +3,14 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+tokens = input_str.split(' ')
 
-# Replace this with your code
+# repeat forever:
+    # read input
+    # tokenize input
+    # if the first token is "q":
+        # quit
+
+    # else:
+        # decide which math function to call based on first token
+    # If first token is pow, call power function with other 2 tokens
